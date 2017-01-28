5b8690f0f6b2ce0a3356023084d8bd327ff3d8c6
Vladimir Nimchyn <nima.vova@gmail.com>
2017-01-28 08:07:40 +0200

    create local static information

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,15 +5,27 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="welcome")
-     */
+
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array('data' => "data"));
+
+//        $t = $this->get('translator')->trans('tmp');
+
+  //      return new Response($t);
+
+
+         return $this->render('index.html.twig', array('data' => "data"));
+    }
+
+
+    public function informationAction(Request $request)
+    {
+
+        return $this->render('index.html.twig', array('data' => "data"));
     }
 }
