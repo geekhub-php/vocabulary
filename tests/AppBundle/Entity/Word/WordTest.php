eec740c5f045ab68ef08737d7eed3febdd1e6200
Vladimir Nimchyn <nima.vova@gmail.com>
2017-02-15 09:11:20 +0200

    Start developing a test database is not yet ready

---
+++ tests/AppBundle/Entity/Word/WordTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\AppBundle\Entity\Word;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class WordTest extends WebDoctrineTestCase
+{
+    public function testGo()
+    {
+        
+    }
+}
+
